3dffdaa3c1879e144889729e1d35b70f46de3252
José Duda da Silva Junior <dudajr.net@outlook.com>
2026-03-11 16:37:08 -0300

    feat(IMC): Adição do projeto IMC

---
+++ Models/AppState.cs
@@ -0,0 +1,13 @@
+namespace Imc.Models;
+
+public static class AppState
+{
+    public static List<ImcModel> History {get; set;} = new();
+
+    public static event EventHandler<EventArgs> OnChanged;
+
+    public static void NotifyChanges(object? sender, EventArgs args)
+    {
+        OnChanged.Invoke(sender, args);
+    }
+}
